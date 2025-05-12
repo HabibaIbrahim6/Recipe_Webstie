896241c983f4b330ec44ddf447e48817e1011891
Shahd132 <162301748+Shahd132@users.noreply.github.com>
2025-05-12 16:30:21 +0300

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import (signup, login_view, create_recipe,
-    get_all_recipes, get_recipe_details,)
+    get_all_recipes, get_recipe_details,list_favorites,add_to_favorites,toggle_favorite)
 
 urlpatterns = [
     path('signup/', signup),
@@ -9,4 +9,8 @@ urlpatterns = [
     path('recipes/add/', create_recipe),
     path('recipes/', get_all_recipes),
     path('recipes/<int:recipe_id>/', get_recipe_details),
+    path('favorites/add/<int:recipe_id>/',add_to_favorites),
+    path('favorites/toggle/', toggle_favorite),
+    path('favorites/', list_favorites),
 ]
+
